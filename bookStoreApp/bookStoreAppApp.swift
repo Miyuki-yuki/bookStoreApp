e2c7d094309862243a76d8305f79ed3027e5a1af
Miyuki <87884079+miyuki-KK@users.noreply.github.com>
2023-10-28 23:16:17 +0900

    Initial Commit

---
+++ bookStoreApp/bookStoreAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  bookStoreAppApp.swift
+//  bookStoreApp
+//
+//  Created by Miyuki Kikuchi on 28/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct bookStoreAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
